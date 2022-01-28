fa11c469d53662c70917516cd34a1d49495a4762
braunsonY <you18012@byui.edu>
2022-01-28 14:37:20 -0700

    week 4 or 5 update

--- week_3/server.js
+++ week_3/server.js
@@ -1,12 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const port = 3000;
 
 const app = express();
 
+
+
+app.use(bodyParser.json());
+
 app.get('/', (req, res)=>{
     res.send("Hello browser")
 }); 
+
+app.post('/login', (req,res)=> {
+    if(req.body.userName =="you18012@byui.edu" && req.body.password =="33516a79199bf858d379a392d244001e"){
+        res.send("Welcome!")
+    } else{
+        res.send("Who are you?")
+    }
+});
+
 
 app.listen(port, ()=>{}); 
 

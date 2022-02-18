7de92f72404666a324ea57f330ac044a93e5cdda
braunsonY <you18012@byui.edu>
2022-02-18 13:26:08 -0700

    week 7

--- weekly_homework/server.js
+++ weekly_homework/server.js
@@ -1,30 +1,42 @@
+
+
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const https = require("https")
+const md5 = require("md5")
 const port = 3000;
-
 const app = express();
-
-var md5 = require('md5');
+const fs = require('fs')
  
-console.log(md5('message'));
-
 app.use(bodyParser.json());
 
 app.get('/', (req, res)=>{
     res.send("Hello browser")
 }); 
 
-app.post('/login', (req,res)=> {
-    if(req.body.userName =="you18012@byui.edu" && req.body.password =="33516a79199bf858d379a392d244001e"){
-        res.send("Welcome!")
-    } else{
-        res.send("Who are you?")
-    }
-});
+https
+  .createServer(
+    {
+      key: fs.readFileSync("server.key"),
+      cert: fs.readFileSync("server.cert"),
+      passphrase: ("P@ssw0rd")
+    },
+    app
+  )
+  .listen(3000, () => {
+    console.log("Listening...");
+  });
 
 
-app.listen(port, ()=>{}); 
 
-//note 
-
+app.post("/login", (req, res) => {
+  console.log(JSON.stringify(req.body));
+  if (
+    req.body.userName == "you18012@byui.edu" &&
+    md5(req.body.password) == "33516a79199bf858d379a392d244001e"
+  ) {
+    res.send("Welcome!");
+  } else {
+    res.send("Who are you?");
+  }
+});  

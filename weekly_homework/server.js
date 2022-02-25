280c77df7f0e2c15c4244545c309708ebd23eb83
braunsonY <you18012@byui.edu>
2022-02-25 13:33:41 -0700

    fixed port more

--- weekly_homework/server.js
+++ weekly_homework/server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const https = require("https")
 const md5 = require("md5")
-const port = 3000;
+const port = 443;
 const app = express();
 const fs = require('fs')
  

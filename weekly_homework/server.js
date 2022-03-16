261e575c2f1102e78a62da3a4f514d6a97aa1558
braunsonY <you18012@byui.edu>
2022-03-16 13:47:04 -0600

    worked on invalid log in attempts

--- weekly_homework/server.js
+++ weekly_homework/server.js
@@ -7,6 +7,8 @@ const port = 443;
 const app = express();
 const fs = require('fs')
 
+let invalidloginAttempts=0; 
+
 app.use(express.static('public'));
  
 app.use(bodyParser.json());
@@ -29,12 +31,19 @@ https
 
 app.post("/login", (req, res) => {
   console.log(JSON.stringify(req.body));
+  if(invalidloginAttempts>=5){
+      res.status(401);//unauthorized
+      res.send("Max password attempts reached");
+  }
   if (
     req.body.userName == "you18012@byui.edu" &&
     md5(req.body.password) == "33516a79199bf858d379a392d244001e"
   ) {
     res.send("Welcome!");
   } else {
+    invalidloginAttempts+=1;
+    console.log (invalidloginAttempts + 'invalid attempts. ')
+    res.status(401);//unauthorized
     res.send("Who are you?");
   }
 });  
